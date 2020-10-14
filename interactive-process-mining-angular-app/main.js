3f75c4bdb0c0d198da7c33d58eae3d02b7203201
schuster-daniel <daniel.schuster@fit.fraunhofer.de>
2020-10-14 13:20:32 +0200

    init commit

---
+++ interactive-process-mining-angular-app/main.js
@@ -0,0 +1,45 @@
+const {app, BrowserWindow} = require('electron')
+const url = require("url");
+const path = require("path");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    minHeight: 600,
+    minWidth: 800,
+
+    width: 1280,
+    height: 800
+
+  })
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, `/dist/index.html`),
+    protocol: "file:",
+    slashes: true
+  }))
+
+  //win.webContents.openDevTools()
+
+  win.on('closed', function () {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  //On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+    //macOS specific
+    if (win === null) {
+      createWindow()
+    }
+  }
+)
