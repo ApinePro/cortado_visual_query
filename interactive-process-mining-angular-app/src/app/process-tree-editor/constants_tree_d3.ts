ffc44503063e1861dfc16f3549c02d244f388928
schuster-daniel <daniel.schuster@fit.fraunhofer.de>
2020-10-15 15:09:42 +0200

    improved tree editor

--- interactive-process-mining-angular-app/src/app/process-tree-editor/constants_tree_d3.ts
+++ interactive-process-mining-angular-app/src/app/process-tree-editor/constants_tree_d3.ts
@@ -1,1 +1,1 @@
-export const tree_operator_height_width = 35;
+export const tree_node_height_width = 35;
--- interactive-process-mining-angular-app/src/app/process-tree-editor/process-tree-editor.component.ts
+++ interactive-process-mining-angular-app/src/app/process-tree-editor/process-tree-editor.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit, ViewChild, AfterViewInit, ElementRef, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewChild, AfterViewInit, ElementRef, ViewEncapsulation, HostListener} from '@angular/core';
 import * as d3 from "d3";
-import {tree_operator_height_width} from "./constants_tree_d3";
+import {tree_node_height_width} from "./constants_tree_d3";
+import {root} from "rxjs/internal-compatibility";
 
 @Component({
   selector: 'app-process-tree-editor',
@@ -14,31 +15,41 @@ export class ProcessTreeEditorComponent implements OnInit, AfterViewInit {
 
   }
 
-  @ViewChild("d3Container") divView: ElementRef;
+  @ViewChild("d3svg") svgElem: ElementRef;
+  @ViewChild("d3container") d3ContainerElem: ElementRef;
+
 
   ngOnInit(): void {
-
   }
 
-  width = 100;
-  height = 100;
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.plot(this.root);
+  }
 
   plot = function (root) {
     console.log("plot")
-    const svg = d3.select("#d3Container").append("svg").attr('width', "100%").attr('height', "100%");
+    const svg = d3.select("#d3-svg")
 
 
-    var treeLayout = d3.tree();
-    treeLayout.size([300, 100]);
+    console.log(this.d3ContainerElem.nativeElement.offsetWidth)
+    console.log(this.d3ContainerElem.nativeElement.offsetHeight)
+
+
+    const treeLayout = d3.tree();
+    treeLayout.size([this.d3ContainerElem.nativeElement.offsetWidth, this.d3ContainerElem.nativeElement.offsetHeight - tree_node_height_width]);
+
     treeLayout(root);
 
     console.log(root.descendants());
     console.log(root.links());
 
-    svg.selectAll('node')
+    const nodeGroups = svg.selectAll('node')
       .data(root.descendants())
       .enter()
-      .append('rect')
+      .append("g")
+
+    nodeGroups.append('rect')
       .classed('node-operator', function (d) {
         return d.data.operator !== null
       })
@@ -46,48 +57,50 @@ export class ProcessTreeEditorComponent implements OnInit, AfterViewInit {
         return d.data.label !== null
       })
       .attr('x', function (d) {
-        return d.x;
+        return d.x - tree_node_height_width / 2;
       })
       .attr('y', function (d) {
         return d.y;
       })
-      .attr('width', tree_operator_height_width)
-      .attr('height', tree_operator_height_width)
+      .attr('width', tree_node_height_width)
+      .attr('height', tree_node_height_width)
       .attr('stroke', 'gray')
       .attr('stroke-width', '2')
-    //.attr('fill', 'none')
 
-    //add node text
-    svg.selectAll('node')
-      .data(root.descendants())
-      .enter()
-      .append("text")
-      .attr("fill", "blue")
+    nodeGroups.append("text")
+      .attr("fill", "white")
       .attr("font-size", "1em")
       .attr("text-anchor", "middle")
       .attr("dominant-baseline", "middle")
       .attr('x', function (d) {
-        return d.x + tree_operator_height_width / 2;
+        return d.x;
       })
       .attr('y', function (d) {
-        return d.y + tree_operator_height_width / 2 +3;
+        return d.y + tree_node_height_width / 2 + 3;
       })
       .text(function (d) {
         if (d.data.operator) return d.data.operator;
         if (d.data.label) return d.data.label;
       })
 
+    nodeGroups.selectAll(".node-visible-activity").attr('x', function (d) {
+      return d.x - Math.max(tree_node_height_width, this.nextSibling.getComputedTextLength() + 10) / 2;
+    }).attr("width", function () {
+      return Math.max(tree_node_height_width, this.nextSibling.getComputedTextLength() + 10);
+    })
+
+
     // add edges
     svg.selectAll('link').data(root.links()).enter()
       .append('line').attr('class', 'link')
       .attr('x1', function (d) {
-        return d.source.x + tree_operator_height_width / 2
+        return d.source.x
       })
       .attr('y1', function (d) {
-        return d.source.y + tree_operator_height_width
+        return d.source.y + tree_node_height_width
       })
       .attr('x2', function (d) {
-        return d.target.x + tree_operator_height_width / 2
+        return d.target.x
       })
       .attr('y2', function (d) {
         return d.target.y
@@ -95,45 +108,70 @@ export class ProcessTreeEditorComponent implements OnInit, AfterViewInit {
       .attr('stroke', 'gray');
   }
 
-  ngAfterViewInit() {
-    const root = {
-      operator: '\u2715',
-      label: null,
-      children: [
-        {
-          operator: '\u21BA',
-          label: null,
+  root = {
+    operator: '\u2715',
+    label: null,
+    children: [
+      {
+        operator: '\u21BA',
+        label: null,
+        children: [{
+          operator: null,
+          label: "a",
           children: []
-        }, {
-          operator: '\u2227',
-          label: null,
-          children: []
-        }, {
-          operator: '\u2192',
-          label: null,
-          children: [
-            {
-              operator: null,
-              label: "a",
-              children: []
-            },
-            {
-              operator: null,
-              label: "b",
-              children: []
-            },
-            {
-              operator: null,
-              label: "long activity name c",
-              children: []
-            }
-          ]
-        }
-      ]
-    };
+        },
+          {
+            operator: null,
+            label: "b",
+            children: []
+          }]
+      }, {
+        operator: '\u2227',
+        label: null,
+        children: [
+          {
+            operator: null,
+            label: "a",
+            children: []
+          },
+          {
+            operator: null,
+            label: "b",
+            children: []
+          },
+          {
+            operator: null,
+            label: "long activity name c",
+            children: []
+          }
+        ]
+      }, {
+        operator: '\u2192',
+        label: null,
+        children: [
+          {
+            operator: null,
+            label: "a",
+            children: []
+          },
+          {
+            operator: null,
+            label: "b",
+            children: []
+          },
+          {
+            operator: null,
+            label: "long activity name c",
+            children: []
+          }
+        ]
+      }
+    ]
+  };
 
-    console.log(d3.hierarchy(root));
-    this.plot(d3.hierarchy(root));
+  ngAfterViewInit() {
+    console.log(d3.hierarchy(this.root));
+    this.plot(d3.hierarchy(this.root));
   }
 
 
