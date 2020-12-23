32dcd267dd072e703d4f5dc5f1d8b27746209f9b
schuster-daniel <daniel.schuster@fit.fraunhofer.de>
2020-12-23 17:21:35 +0100

    added auto reload of backend

--- backend/main.py
+++ backend/main.py
@@ -133,4 +133,4 @@ def download_test(d: ConvertPtToX):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
