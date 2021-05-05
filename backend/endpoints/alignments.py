5e923f03b8d0b5ad80974f0621ba173842f274f1
schuster-daniel <daniel.schuster@fit.fraunhofer.de>
2021-05-05 09:56:38 +0200

    removes deprecated import

--- backend/endpoints/alignments.py
+++ backend/endpoints/alignments.py
@@ -2,10 +2,11 @@ from functools import lru_cache
 
 from pm4py.algo.conformance.alignments.algorithm import apply_trace as get_alignment
 from pm4py.objects.conversion.process_tree.converter import apply as convert_pt_to_petri_net
-from pm4py.objects.log.log import Trace, Event
+from pm4py.objects.log.obj import Trace, Event
 from pm4py.objects.process_tree.process_tree import ProcessTree
 from backend.backend_utilities.process_tree_conversion import dict_to_process_tree
 from pm4py.objects.petri.align_utils import STD_MODEL_LOG_MOVE_COST, SKIP
+# from pm4py.algo.conformance.alignments.process_tree.algorithm import apply as tree_alignment
 
 
 # @lru_cache(maxsize=None)
