44dbb556cb4022667442dbc1ce28723e824d0734
schuster-daniel <daniel.schuster@fit.fraunhofer.de>
2021-05-07 15:34:39 +0200

    removes log statements

--- interactive-process-mining-angular-app/src/app/components/side-bar/side-bar.component.ts
+++ interactive-process-mining-angular-app/src/app/components/side-bar/side-bar.component.ts
@@ -21,55 +21,51 @@ export class SideBarComponent implements OnInit {
   @ViewChild('fileUploadEventLog') fileUploadEventLog: ElementRef;
   @ViewChild('fileUploadProcessTree') fileUploadProcessTree: ElementRef;
 
-  importEventLog() {
-    console.log('file upload click');
+  importEventLog(): void {
     this.fileUploadEventLog.nativeElement.click();
   }
 
-  handleSelectedEventLogFile(e) {
-    console.log(e)
-
+  handleSelectedEventLogFile(e): void {
     const taskDescription = 'Loading/parsing event log';
-    this.backgroundTaskInfoService.setNewTask(taskDescription)
+    this.backgroundTaskInfoService.setNewTask(taskDescription);
 
     const fileList: FileList = e.target.files;
     if (fileList.length > 0) {
       console.log(fileList[0]);
       const fileName = fileList[0].name;
-      //TODO make it also work with browser by uploading the file instead of just the file path
+      // TODO make it also work with browser by uploading the file instead of just the file path
       /*this.backendService.uploadEventLog$(fileList[0]).subscribe(() => {
         console.log('success?');
       });*/
       this.backendService.loadEventLogFromFilePath(fileList[0]['path']).subscribe(res => {
-        console.log('Event log ' + fileName + ' loaded');
+        // console.log('Event log ' + fileName + ' loaded');
         this.backgroundTaskInfoService.removeTask(taskDescription);
         this.sharedDataService.loadedEventLog = fileName;
-      })
+      });
     }
     // reset form
     this.fileUploadEventLog.nativeElement.value = '';
   }
 
-  handleSelectedProcessTreeFile(e) {
-    console.log(e);
+  handleSelectedProcessTreeFile(e): void {
+    // console.log(e);
     const fileList: FileList = e.target.files;
     if (fileList.length > 0) {
-      console.log(fileList[0]);
-      //const fileName = fileList[0].name;
+      // console.log(fileList[0]);
       this.backendService.loadProcessTreeFromFilePath(fileList[0]['path']);
     }
     this.fileUploadProcessTree.nativeElement.value = '';
   }
 
-  exportTreeAsPTML() {
+  exportTreeAsPTML(): void {
     this.backendService.downloadCurrentTreeAsPTML();
   }
 
-  exportTreeAsPNML() {
+  exportTreeAsPNML(): void {
     this.backendService.downloadCurrentTreeAsPNML();
   }
 
-  importTreeFromPTML() {
+  importTreeFromPTML(): void {
     this.fileUploadProcessTree.nativeElement.click();
   }
 }
