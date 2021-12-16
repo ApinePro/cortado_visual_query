5cf1f95f8c41e5791a19a2f0ff5402f9b60b0515
niklas.foecking <niklas.foecking@fit.fraunhofer.de>
2021-12-16 08:52:16 +0000

    26-timeout-for-alignment-computation

---
+++ backend/backend_utilities/configuration/repository.py
@@ -0,0 +1,81 @@
+import abc
+import json
+from pathlib import Path
+from appdirs import user_config_dir
+import os
+import errno
+
+CONFIG_FILENAME = 'config.json'
+DEFAULT_TIMEOUT = 30
+APP_NAME = 'cortado'
+COMPANY_NAME = 'Fraunhofer'
+
+
+class Configuration:
+    timeout_cvariant_alignment_computation: int
+
+    def __init__(self, timeout_cvariant_alignment_computation):
+        self.timeout_cvariant_alignment_computation = timeout_cvariant_alignment_computation
+
+
+class ConfigurationRepository(abc.ABC):
+    @abc.abstractmethod
+    def save_configuration(self, config: Configuration):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_configuration(self) -> Configuration:
+        raise NotImplementedError
+
+
+class FileBasedConfigurationRepository(ConfigurationRepository):
+    def save_configuration(self, config: Configuration):
+        directory = user_config_dir(APP_NAME, COMPANY_NAME)
+        filename = os.path.join(directory, CONFIG_FILENAME)
+        self.__create_dir_if_not_exists(filename)
+
+        with open(filename, 'w+', encoding='utf-8') as f:
+            json.dump(config.__dict__, f, ensure_ascii=False, indent=4)
+
+    def get_configuration(self) -> Configuration:
+        directory = user_config_dir(APP_NAME, COMPANY_NAME)
+        if not Path(os.path.join(directory, CONFIG_FILENAME)).is_file():
+            return Configuration(timeout_cvariant_alignment_computation=DEFAULT_TIMEOUT)
+
+        with open(os.path.join(directory, CONFIG_FILENAME), 'r', encoding='utf-8') as f:
+            data = json.load(f, encoding='utf-8')
+            return Configuration(timeout_cvariant_alignment_computation=data['timeout_cvariant_alignment_computation'])
+
+    # see https://stackoverflow.com/questions/12517451/automatically-creating-directories-with-file-output
+    @staticmethod
+    def __create_dir_if_not_exists(filename: str):
+        if not os.path.exists(os.path.dirname(filename)):
+            try:
+                os.makedirs(os.path.dirname(filename))
+            except OSError as exc:  # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+
+
+class ConfigurationRepositoryCacheProxy(ConfigurationRepository):
+    def __init__(self, config_repo: ConfigurationRepository):
+        self.config_repo = config_repo
+        self.cached_config = None
+
+    def save_configuration(self, config: Configuration):
+        self.config_repo.save_configuration(config)
+        self.cached_config = config
+
+    def get_configuration(self) -> Configuration:
+        if self.cached_config is None:
+            self.cached_config = self.config_repo.get_configuration()
+
+        return self.cached_config
+
+
+class ConfigurationRepositoryFactory:
+    repo = ConfigurationRepositoryCacheProxy(FileBasedConfigurationRepository())
+
+    @staticmethod
+    def get_config_repository() -> ConfigurationRepository:
+        return ConfigurationRepositoryFactory.repo
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ProcessTreeEditorComponent } from './components/process-tree-editor/process-tree-editor.component';
 import {StrFilterPipe} from './pipes/str-filter/str-filter.pipe';
 import { FormsModule } from '@angular/forms';
-import { NgxFileDropModule } from 'ngx-file-drop'
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 import { VariantExplorerComponent } from './components/variant-explorer/variant-explorer.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
@@ -21,6 +21,7 @@ import { VariantInfoComponent } from './components/variant-explorer/variant-info
 import { VariantSelectionButtonComponent } from './components/variant-explorer/variant-selection-button/variant-selection-button.component';
 import { GoldenLayoutComponentService } from './services/goldenLayoutService/golden-layout-component.service';
 import { GoldenLayoutHostComponent } from './components/golden-layout-host/golden-layout-host.component';
+import { SettingsComponent } from './components/settings/settings.component';
 import { DropZoneDirective } from './directives/drop-zone/drop-zone.directive';
 
 @NgModule({
@@ -37,6 +38,7 @@ import { DropZoneDirective } from './directives/drop-zone/drop-zone.directive';
     VariantSelectionButtonComponent,
     ActivityOverviewComponent,
     ProcessTreeEditorComponent,
+    SettingsComponent,
     DropZoneComponent,
     DropZoneDirective
   ],
