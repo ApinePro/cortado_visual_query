d8f7559ff44661bf1a0fbc9f322807c5d717a9ee
niklas.foecking <niklas.foecking@fit.fraunhofer.de>
2022-01-26 12:21:01 +0000

    108-adding-the-warning-icon-into-the-performance-calculation-tooltip

--- frontend/src/app/services/performance.service.ts
+++ frontend/src/app/services/performance.service.ts
@@ -181,7 +181,7 @@ export class PerformanceService {
     }
 
     if (fitness !== undefined && fitness < 1) {
-      tooltipText = `${tooltipText}<hr class="performance-tooltip-hr">Unfitting traces: possibly unreliable model performance values!<br>Fitness: ${fitness.toFixed(
+      tooltipText = `${tooltipText}<hr class="performance-tooltip-hr"><i class="bi bi-exclamation-triangle-fill text-warning"> Unfitting traces: possibly unreliable model performance values!</i><br>Fitness: ${fitness.toFixed(
         2
       )}`;
     }
