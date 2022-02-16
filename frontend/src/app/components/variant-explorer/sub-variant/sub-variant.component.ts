87a13c2ebcab63c285e718ed2e6c2f02ae4a9df3
minh.nghia.phan <minh.nghia.phan@fit.fraunhofer.de>
2022-02-16 17:42:29 +0700

    Make line thickness equal diameter of the circles subvariants

--- frontend/src/app/components/variant-explorer/sub-variant/sub-variant.component.ts
+++ frontend/src/app/components/variant-explorer/sub-variant/sub-variant.component.ts
@@ -60,7 +60,8 @@ export class SubVariantComponent implements AfterViewInit {
       .attr('x1', ([_, d]) => xScale(d[0][0]))
       .attr('x2', ([_, d]) => xScale(d[1][0]))
       .attr('y1', ([_, d]) => yScale(d[0][1]))
-      .attr('y2', ([_, d]) => yScale(d[1][1]));
+      .attr('y2', ([_, d]) => yScale(d[1][1]))
+      .attr("stroke-width", ([_,d]) => 2*Constants.POINT_RADIUS);
 
     const circles = g
       .selectAll('circle')
