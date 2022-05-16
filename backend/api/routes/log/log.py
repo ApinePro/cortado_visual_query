d8085656a7c7d38538a0308b348e051e5a370e96
gero.kolhof <gero.kolhof@fit.fraunhofer.de>
2022-05-16 09:14:26 +0000

    Resolve "Log cache file not found"

--- backend/api/routes/log/log.py
+++ backend/api/routes/log/log.py
@@ -1,11 +1,11 @@
 import pickle
 
-import cache.log_cache as log_cache
 from cortado_core.utils.timestamp_utils import TimeUnit
 from endpoints.load_event_log import (calculate_event_log_properties,
                                       get_time_granularity)
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
+import cache.log_cache as log_cache
 
 router = APIRouter(
     tags=["Log"],
