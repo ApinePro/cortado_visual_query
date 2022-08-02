98775d6378bc0fd9bb01eba2ec1dc8a0457106af
Niklas Foecking <niklas.foecking@fit.fraunhofer.de>
2022-08-02 10:21:33 +0200

    ft-228: Fix issues with button positioning after merge of refactoring mr

--- frontend/src/app/components/variant-explorer/variant/subcomponents/infix-selection-controlls/infix-selection-controlls.component.ts
+++ frontend/src/app/components/variant-explorer/variant/subcomponents/infix-selection-controlls/infix-selection-controlls.component.ts
@@ -3,24 +3,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Variant } from 'src/app/objects/Variants/variant';
 
 @Component({
-  selector: 'app-infix-selection-controlls',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: '[app-infix-selection-controlls]',
   templateUrl: './infix-selection-controlls.component.html',
   styleUrls: ['./infix-selection-controlls.component.css'],
 })
 export class InfixSelectionControllsComponent {
   constructor() {}
 
+  isAnyInfixSelected = true;
+
   @Input()
   variant: Variant;
 
   @Input()
   variantDrawer: VariantDrawerDirective;
-
-  @Input()
-  traceInfixSelectionMode: boolean = false;
-
-  @Input()
-  performanceMode: boolean = false;
 
   @Output()
   public selectTraceInfix = new EventEmitter<Variant>();
