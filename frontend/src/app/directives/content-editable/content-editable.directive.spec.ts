ae40e63f7228cb8a4562c2cae7cb2145650dab46
Michael Martini <michael.martini1@rwth-aachen.de>
2022-09-05 14:00:54 +0200

    Formed

--- frontend/src/app/directives/content-editable/content-editable.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { TestBed } from '@angular/core/testing';
-import { ContentEditableDirective } from './content-editable.directive';
-
-describe('Directive: ContentEditableDirective', () => {
-  it('should create an instance', () => {
-    const directive = TestBed.createComponent(ContentEditableDirective);
-    expect(directive).toBeTruthy();
-  });
-});
