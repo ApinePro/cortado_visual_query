41a85e1c113a36885214d8248781e78e3bc5e5f5
Michael Martini <michael.martini1@rwth-aachen.de>
2022-09-27 14:38:03 +0200

    Update filter-options.component.ts

--- frontend/src/app/components/variant-miner/filter-options/filter-options.component.ts
+++ frontend/src/app/components/variant-miner/filter-options/filter-options.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './filter-options.component.html',
   styleUrls: ['./filter-options.component.css'],
 })
-export class FilterOptionsComponent implements OnInit {
+export class FilterOptionsComponent {
   constructor() {}
 
   @Input()
