f5421ee8fc51f1952152e1163771aeb6cfff52c6
Niklas Foecking <niklas.foecking@fit.fraunhofer.de>
2022-09-28 10:33:58 +0200

    ft-261: Delete variant on click

--- frontend/src/app/components/variant-explorer/variant-delete-button/variant-delete-button.component.ts
+++ frontend/src/app/components/variant-explorer/variant-delete-button/variant-delete-button.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { VariantElement } from 'src/app/objects/Variants/variant_element';
+import { VariantService } from 'src/app/services/variantService/variant.service';
 
 @Component({
   selector: 'app-variant-delete-button',
@@ -6,7 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./variant-delete-button.component.css'],
 })
 export class VariantDeleteButtonComponent {
+  @Input()
+  private variant: VariantElement;
+
+  constructor(private variantService: VariantService) {}
+
   deleteVariant() {
-    console.log('delete variant');
+    const bids = this.variantService.variants
+      .filter((v) => v.variant === this.variant)
+      .map((v) => v.bid);
+
+    this.variantService.deleteVariants(bids);
   }
 }
