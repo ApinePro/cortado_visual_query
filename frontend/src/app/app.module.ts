024bdceb4d06a0b11ad472afd5caaa601db0f82f
Niklas Foecking <niklas.foecking@fit.fraunhofer.de>
2022-10-17 13:14:05 +0200

    Fix error after merge conflict

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -62,13 +62,10 @@ import { InfixSelectionControllsComponent } from './components/variant-explorer/
 import { ZoomFieldComponent } from './components/zoom-field/zoom-field.component';
 import { VariantExplorerSidebarComponent } from './components/variant-explorer/variant-explorer-sidebar/variant-explorer-sidebar.component';
 import { EditorZoneComponent } from './components/editor-zone/editor-zone.component';
-<<<<<<< HEAD
 import { VariantDeleteButtonComponent } from './components/variant-explorer/variant-delete-button/variant-delete-button.component';
-=======
 import { ELECTRON_SERVICE } from './tokens';
 import { ElectronService } from './services/electronService/electron.service';
 
->>>>>>> integration
 @NgModule({
   declarations: [
     AppComponent,
