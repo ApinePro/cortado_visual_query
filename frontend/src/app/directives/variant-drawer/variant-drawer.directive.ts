b3397222480be48bd73c76e0f88f6ec061787fbc
Niklas Foecking <niklas.foecking@fit.fraunhofer.de>
2022-10-25 15:09:59 +0200

    ft-268: Draw loop groups

--- frontend/src/app/directives/variant-drawer/variant-drawer.directive.ts
+++ frontend/src/app/directives/variant-drawer/variant-drawer.directive.ts
@@ -608,33 +608,35 @@ export class VariantDrawerDirective
           (VARIANT_Constants.FONT_SIZE + VARIANT_Constants.MARGIN_Y);
     }
 
-    let truncated = false;
-    let dy = 0;
-    leafNode.activity.forEach((a, _i) => {
-      const tspan = activityText
-        .append('tspan')
-        .attr('x', width / 2)
-        .attr('y', y + dy)
-        .classed('cursor-pointer', !this.traceInfixSelectionMode || actionable)
-        .text(a);
-
-      dy += VARIANT_Constants.FONT_SIZE + VARIANT_Constants.MARGIN_Y;
-      tspan.attr(
-        'height',
-        VARIANT_Constants.FONT_SIZE + VARIANT_Constants.MARGIN_Y
+    parent
+      .append('path')
+      .attr('fill', textcolor)
+      .attr(
+        'd',
+        'M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192Zm3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z'
+      )
+      .attr(
+        'transform',
+        'translate(' + (width / 2 - 8) + ',' + VARIANT_Constants.MARGIN_Y + ')'
       );
 
-      const maxWidth =
-        loopGroup.getWidth() -
-        loopGroup.getHeadLength() * 2 -
-        VARIANT_Constants.MARGIN_X;
-      const tr = this.wrapInnerLabelText(tspan, a, maxWidth);
-      truncated ||= tr;
-    });
+    let label = leafNode.activity[0];
+    const tspan = activityText
+      .append('tspan')
+      .attr('x', width / 2)
+      .attr('y', y + VARIANT_Constants.FONT_SIZE - VARIANT_Constants.MARGIN_Y)
+      .classed('cursor-pointer', !this.traceInfixSelectionMode || actionable)
+      .text(label);
+
+    const maxWidth =
+      loopGroup.getWidth() -
+      loopGroup.getHeadLength() * 2 -
+      VARIANT_Constants.MARGIN_X;
+    const truncated = this.wrapInnerLabelText(tspan, label, maxWidth);
 
     if (truncated) {
       activityText
-        .attr('title', leafNode.activity.join(';'))
+        .attr('title', leafNode.activity[0])
         .attr('data-bs-toggle', 'tooltip');
     }
 
