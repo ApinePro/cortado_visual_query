6b7ed47f340c49d222fe163426585ec984221f4a
Niklas Foecking <niklas.foecking@fit.fraunhofer.de>
2022-10-27 10:20:31 +0200

    ft-279: Re-add query-variants handler such that it can process requests

--- backend/api/routes/api.py
+++ backend/api/routes/api.py
@@ -13,7 +13,7 @@ from api.routes.performance import (
     variantPerformance,
 )
 from api.routes.process_tree import discoverTree, modifyTree, treeString
-from api.routes.variants import variants
+from api.routes.variants import variants, queryVariant
 
 router = APIRouter()
 router.include_router(log.router)
@@ -30,4 +30,5 @@ router.include_router(discoverTree.router)
 router.include_router(modifyTree.router)
 router.include_router(treeString.router)
 router.include_router(variants.router)
+router.include_router(queryVariant.router)
 
