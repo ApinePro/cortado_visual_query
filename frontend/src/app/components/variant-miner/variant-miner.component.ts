1e6629a228e04b9bccfcbfd3c09a8dc9b6a420df
Michael Martini <michael.martini1@rwth-aachen.de>
2022-11-08 13:47:39 +0100

    Fixed ticks for Filter

--- frontend/src/app/components/variant-miner/variant-miner.component.ts
+++ frontend/src/app/components/variant-miner/variant-miner.component.ts
@@ -653,7 +653,7 @@ export class VariantMinerComponent
 
   private set_interval_filter_configs() {
     this.kFilter.set_config(3, this.maxSize);
-    this.supFilter.set_config(this.minsup, this.maxSup);
+    this.supFilter.set_config(this.minsup, this.maxSup, Math.round((this.maxSup - this.minsup) / 10), Math.round((this.maxSup - this.minsup) / 20));
     this.idFilter.set_config(0, this.variantPatterns.length);
     this.cpConfFilter.set_config(0, 1);
     this.supConfFilter.set_config(0, 1);
@@ -915,7 +915,11 @@ export class IntervalFilter {
   defaultLow: number;
   defaultHigh: number;
 
-  set_config(floor, ceil) {
+  set_config(floor, ceil, tickStep?, tickValueStep?) {
+
+    this.tickStep = tickStep ? tickStep : this.tickStep;
+    this.tickValueStep = tickValueStep ? tickValueStep : this.tickStep;
+
     this.config = {
       floor: floor,
       ceil: ceil,
@@ -924,7 +928,7 @@ export class IntervalFilter {
       tickStep: this.tickStep,
       tickValueStep: this.tickValueStep,
       step: this.step,
-    };
+    };3
 
     this.defaultLow = floor;
     this.defaultHigh = ceil;
