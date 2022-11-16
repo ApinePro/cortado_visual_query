12a782e95910063aa0b83f5ab6832745b5e402bc
Lars Dietrich <lars.dietrich@fit.fraunhofer.de>
2022-11-16 12:55:18 +0100

    Add conformance property to process tree

--- backend/backend_utilities/process_tree_conversion.py
+++ backend/backend_utilities/process_tree_conversion.py
@@ -13,15 +13,15 @@ TAU_CHAR = "\u03C4"
 
 
 def process_tree_to_dict(
-    pt: ProcessTree, frozen_subtrees: List[ProcessTree] = [], performance={}
+    pt: ProcessTree, frozen_subtrees: List[ProcessTree] = [], performance={}, conformance=None
 ) -> dict:
     return process_tree_to_dict_rec(
-        pt, frozen_subtrees=frozen_subtrees, performance=performance
+        pt, frozen_subtrees=frozen_subtrees, performance=performance, conformance=conformance
     )
 
 
 def process_tree_to_dict_rec(
-    pt: ProcessTree, frozen_subtrees: List[ProcessTree] = [], performance={}
+    pt: ProcessTree, frozen_subtrees: List[ProcessTree] = [], performance={}, conformance=None
 ) -> dict:
     pt_frozen = False
     for frozen_subtree in frozen_subtrees:
@@ -34,11 +34,33 @@ def process_tree_to_dict_rec(
         "children": [],
         "frozen": pt_frozen,
         "performance": performance.get(str(pt), None),
+        "conformance": None
     }
     for c in pt.children:
         res["children"].append(
-            process_tree_to_dict_rec(c, frozen_subtrees, performance)
+            process_tree_to_dict_rec(
+                c, frozen_subtrees, performance, conformance)
         )
+    if conformance:
+        if res['children']:
+            child_conformance = [child['conformance']
+                                 for child in res['children'] if child['conformance'] is not None]
+            if len(child_conformance) > 0:
+                if pt.operator == Operator.XOR:
+                    # max of childrens conformance
+                    res['conformance'] = max(
+                        child_conformance, key=lambda conf: conf['value'])
+                else:
+                    # mean of childrens conformance
+                    freq_sum = sum(
+                        list(map(lambda conf: conf['weight'], child_conformance)))
+                    res['conformance'] = {
+                        'value': sum(list(map(lambda conf: conf['value'] * conf['weight'], child_conformance))) / freq_sum,
+                        'weight': freq_sum
+
+                    }
+        elif conformance[str(pt)]['value'] is not None:
+            res['conformance'] = conformance[str(pt)]
     return res
 
 
@@ -90,14 +112,16 @@ def dict_to_process_tree(
             operator=__convert_operator_string_from_frontend_for_pm4py_core(
                 pt["operator"]
             ),
-            label=__convert_label_string_from_frontend_for_pm4py_core(pt["label"]),
+            label=__convert_label_string_from_frontend_for_pm4py_core(
+                pt["label"]),
         )
     else:
         subtree = ProcessTree(
             operator=__convert_operator_string_from_frontend_for_pm4py_core(
                 pt["operator"]
             ),
-            label=__convert_label_string_from_frontend_for_pm4py_core(pt["label"]),
+            label=__convert_label_string_from_frontend_for_pm4py_core(
+                pt["label"]),
             parent=res,
         )
         res.children.append(subtree)
@@ -122,7 +146,8 @@ if __name__ == "__main__":
         "label": None,
         "operator": "➔",
         "children": [
-            {"label": "A_SUBMITTED", "operator": None, "children": [], "frozen": False},
+            {"label": "A_SUBMITTED", "operator": None,
+                "children": [], "frozen": False},
             {
                 "label": "A_PARTLYSUBMITTED",
                 "operator": None,
@@ -133,7 +158,8 @@ if __name__ == "__main__":
                 "label": None,
                 "operator": "✕",
                 "children": [
-                    {"label": "τ", "operator": None, "children": [], "frozen": True},
+                    {"label": "τ", "operator": None,
+                        "children": [], "frozen": True},
                     {
                         "label": "A_Completeren aanvraag",
                         "operator": None,
@@ -147,7 +173,8 @@ if __name__ == "__main__":
                 "label": None,
                 "operator": "✕",
                 "children": [
-                    {"label": "τ", "operator": None, "children": [], "frozen": True},
+                    {"label": "τ", "operator": None,
+                        "children": [], "frozen": True},
                     {
                         "label": "W_Completeren aanvraag",
                         "operator": None,
