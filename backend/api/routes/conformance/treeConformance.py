b3af618fb81a0837261691baf8c7fe2c60fede69
Lars Dietrich <lars.dietrich@fit.fraunhofer.de>
2022-11-17 12:54:08 +0100

    Calculate Merged Conformance for Concurrent Variants

--- backend/api/routes/conformance/treeConformance.py
+++ backend/api/routes/conformance/treeConformance.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from fastapi import APIRouter
 from pydantic import BaseModel
 from cortado_core.utils.process_tree import convert_tree
+from cortado_core.utils.cvariants import generate_variants
 
 from cache import cache
 
@@ -27,57 +28,67 @@ async def calculate_tree_conformance(d: InputCalculateConformance):
     all_conf_stats = []
 
     for bid in d.variants:
-        (_, trace, _) = cache.variants[bid]
+        (variant, _, _) = cache.variants[bid]
         # calc alignment
-        trace = list(map(lambda event: event['concept:name'], trace[0]))
-        alignment = calculate_alignment(trace, d.pt, d.infix_type, True)
-        conf_stats = defaultdict(lambda: {'value': None, 'weight': 0})
-        for (log_move, model_move) in alignment['alignment']:
-            model_move = str(model_move)
+        variants = generate_variants(variant.serialize())
+        variant_tree_conformances = []
+        for variant in variants:
+            alignment = calculate_alignment(variant, d.pt, d.infix_type, True)
 
-            if(model_move == '>>'):
-                continue
+            conf_stats = defaultdict(lambda: {'value': None, 'weight': 0})
+            for (log_move, model_move) in alignment['alignment']:
+                model_move = str(model_move)
 
-            if conf_stats[model_move]['value'] is None:
-                conf_stats[model_move]['value'] = 0
+                if(model_move == '>>'):
+                    continue
 
-            conf_stats[model_move]['weight'] += 1
+                if conf_stats[model_move]['value'] is None:
+                    conf_stats[model_move]['value'] = 0
 
-            # when move is properly aligned
-            if(
-                log_move == model_move.rsplit('_', 1)[0] or
-                (
-                    log_move == '>>' and
-                    model_move.rsplit('_', 1)[0] == 'tau'
-                )
-            ):
-                conf_stats[model_move]['value'] += 1
+                conf_stats[model_move]['weight'] += 1
 
-        for conf_stat in conf_stats.values():
-            conf_stat['value'] = conf_stat['value'] / \
-                conf_stat['weight']
+                # when move is properly aligned
+                if(
+                    log_move == model_move.rsplit('_', 1)[0] or
+                    (
+                        log_move == '>>' and
+                        model_move.rsplit('_', 1)[0] == 'tau'
+                    )
+                ):
+                    conf_stats[model_move]['value'] += 1
 
-        all_conf_stats.append(conf_stats)
+            for conf_stat in conf_stats.values():
+                conf_stat['value'] = conf_stat['value'] / conf_stat['weight']
+
+            variant_tree_conformances.append(conf_stats)
+
+        variant_conf_stats = merge_conf_stats(variant_tree_conformances)
+        all_conf_stats.append(variant_conf_stats)
         variants_tree_conformance[bid] = process_tree_to_dict(
-            pt, conformance=conf_stats)
+            pt, conformance=variant_conf_stats)
 
-    merged_conf_stats = {}
-    if len(all_conf_stats) > 1:
-        for key in all_conf_stats[0].keys():
-            values = [conf_stats[key]['value']
-                      for conf_stats in all_conf_stats if conf_stats[key]['value'] is not None]
-            weights = [conf_stats[key]['weight']
-                       for conf_stats in all_conf_stats if conf_stats[key]['value'] is not None]
-            merged_conf_stats[key] = {
-                'value': sum(values) / len(values) if len(values) > 0 else None,
-                'weight': sum(weights)
-            }
-    elif len(all_conf_stats) == 1:
-        merged_conf_stats = all_conf_stats[0]
-
-    pt_dict = process_tree_to_dict(pt, conformance=merged_conf_stats)
+    merged_conf_stat = merge_conf_stats(all_conf_stats)
+    pt_dict = process_tree_to_dict(pt, conformance=merged_conf_stat)
 
     return {
         "merged_conformance_tree": pt_dict,
         "variants_tree_conformance": variants_tree_conformance,
     }
+
+
+def merge_conf_stats(conf_stats: List[dict]):
+    if len(conf_stats) == 1:
+        return conf_stats[0]
+    merged_stats = {}
+    if len(conf_stats) > 1:
+        for key in conf_stats[0].keys():
+            values = [stats[key]['value']
+                      for stats in conf_stats if stats[key]['value'] is not None]
+            weights = [stats[key]['weight']
+                       for stats in conf_stats if stats[key]['value'] is not None]
+            merged_stats[key] = {
+                'value': sum(values) / len(values) if len(values) > 0 else None,
+                'weight': sum(weights)
+            }
+        return merged_stats
+    return None
