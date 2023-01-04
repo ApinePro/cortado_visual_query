0816173d4f911360feb9595d5ee2dbfb5a7c3058
Niklas Foecking <niklas.foecking@fit.fraunhofer.de>
2023-01-04 15:09:27 +0100

    ft-262: Add basic validation for tiebreaker pattern; advanced validation will be implemented later, after the issues in the variant modeler are fixed

--- backend/api/routes/variants/tiebreaker.py
+++ backend/api/routes/variants/tiebreaker.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
 
+from cortado_core.subprocess_discovery.concurrency_trees.cTrees import cTreeOperator
 from cortado_core.tiebreaker.algorithm import apply_tiebreaker_on_variants
-from cortado_core.tiebreaker.pattern import parse_tiebreaker_pattern
-from fastapi import APIRouter
+from cortado_core.tiebreaker.pattern import parse_tiebreaker_pattern, TiebreakerPattern, WILDCARD_MATCH
+from cortado_core.tiebreaker.two_plus_two_free_check import get_wildcard_node
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 import cache.cache
@@ -28,8 +30,16 @@ def apply_tiebreaker(payload: TiebreakerPatterns):
         new_variants[variant] += traces
         n_traces += len(traces)
 
+    if not validate_string_pattern(payload.sourcePattern):
+        raise HTTPException(status_code=400, detail='Source pattern is invalid')
+
+    if not validate_string_pattern(payload.targetPattern):
+        raise HTTPException(status_code=400, detail='Target pattern is invalid')
+
     source_pattern = parse_tiebreaker_pattern(payload.sourcePattern)
     target_pattern = parse_tiebreaker_pattern(payload.targetPattern)
+
+    validate_patterns(source_pattern, target_pattern)
 
     print('SOURCE PATTERN:', str(source_pattern))
     print('TARGET PATTERN:', str(target_pattern))
@@ -67,3 +77,76 @@ def apply_tiebreaker(payload: TiebreakerPatterns):
     }
 
     return res
+
+
+def validate_string_pattern(pattern: str) -> bool:
+    return pattern.count('(') == pattern.count(')')
+
+
+def validate_patterns(source_pattern: TiebreakerPattern, target_pattern: TiebreakerPattern):
+    activities = cache.cache.parameters["activites"]
+    source_activities = get_activities_in_pattern(source_pattern)
+    target_activities = get_activities_in_pattern(target_pattern)
+
+    for source_activity in source_activities:
+        if source_activity not in activities:
+            raise HTTPException(status_code=400, detail=f"Source pattern contains invalid activity '{source_activity}'")
+
+    for target_activity in target_activities:
+        if target_activity not in activities:
+            raise HTTPException(status_code=400, detail=f"Target pattern contains invalid activity '{target_activity}'")
+
+    source_labeled_nodes = [set(p.labels) for p in get_activity_nodes_in_pattern(source_pattern)]
+    target_labeled_nodes = [set(p.labels) for p in get_activity_nodes_in_pattern(target_pattern)]
+
+    for source_labeled_node in source_labeled_nodes:
+        try:
+            target_labeled_nodes.remove(source_labeled_node)
+        except ValueError:
+            raise HTTPException(status_code=400,
+                                detail=f"Node with labels {source_labeled_node} is present in source pattern, but not in target pattern")
+
+    if len(target_labeled_nodes) > 0:
+        raise HTTPException(status_code=400,
+                            detail=f"Node with labels {target_labeled_nodes[0]} is present in target pattern, but not in source pattern")
+
+    source_wc_node = get_wildcard_node(source_pattern)
+    target_wc_node = get_wildcard_node(target_pattern)
+
+    if source_wc_node is None and target_wc_node is not None:
+        raise HTTPException(status_code=400,
+                            detail=f"Target pattern has wildcard (...), which is not present in source pattern")
+
+    if source_wc_node is not None and target_wc_node is None:
+        raise HTTPException(status_code=400,
+                            detail=f"Source pattern has wildcard (...), which is not present in target pattern")
+
+    if source_pattern.operator != cTreeOperator.Concurrent:
+        raise HTTPException(status_code=400,
+                            detail=f"Source pattern without concurrent operator at highest level")
+
+    for child in source_pattern.children:
+        if child.operator is not None and child.operator != WILDCARD_MATCH:
+            raise HTTPException(status_code=400,
+                                detail=f"Source pattern cannot have nested operators")
+
+
+def get_activities_in_pattern(pattern: TiebreakerPattern):
+    activities = set(pattern.labels)
+
+    for child in pattern.children:
+        activities = activities.union(get_activities_in_pattern(child))
+
+    return activities
+
+
+def get_activity_nodes_in_pattern(pattern: TiebreakerPattern):
+    nodes = set()
+
+    if len(pattern.labels) > 0:
+        nodes.add(pattern)
+
+    for child in pattern.children:
+        nodes = nodes.union(get_activity_nodes_in_pattern(child))
+
+    return nodes
