89ca4052f1bf3d6a255453cca1050f63f39f8911
fit-daniel-schuster <daniel.schuster@fit.fraunhofer.de>
2023-01-22 16:04:26 +0100

    update build scripts

--- build_scripts/build_cortado_linux.sh
+++ build_scripts/build_cortado_linux.sh
@@ -4,7 +4,9 @@ cd ./../src/backend
 pip install -r requirements.txt
 python -O -m PyInstaller --noconfirm --clean cortado-backend.spec
 cp -r ./dist/cortado-backend ./../frontend/cortado-backend
+
 cd ./../frontend
+npm install
 npm run electron-builder-app-production-linux
 cp -r ./cortado-backend ./app-dist/linux-unpacked
 rm -r -f ./cortado-backend
