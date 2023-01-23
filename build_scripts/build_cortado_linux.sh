ae9c5d584a28641a981d4d5cee6c9527e9bd33cd
fit-daniel-schuster <daniel.schuster@fit.fraunhofer.de>
2023-01-23 08:41:26 +0100

    update linux build script

--- build_scripts/build_cortado_linux.sh
+++ build_scripts/build_cortado_linux.sh
@@ -2,7 +2,7 @@
 
 cd ./../src/backend
 pip install -r requirements.txt
-python -O -m PyInstaller --noconfirm --clean cortado-backend.spec
+python3 -O -m PyInstaller --noconfirm --clean cortado-backend.spec
 cp -r ./dist/cortado-backend ./../frontend/cortado-backend
 
 cd ./../frontend
