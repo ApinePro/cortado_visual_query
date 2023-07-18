f3d6a01a29623265fbce8fcaee08ca27e90ffdd6
Ariba Siddiqui <ariba.siddiqui0504@gmail.com>
2023-07-18 14:32:29 +0200

    added footer in activity overview

---
+++ src/frontend/src/app/components/activity-overview/info-bar/info-bar.component.ts
@@ -0,0 +1,42 @@
+import { ActivityField } from 'src/app/components/activity-overview/activity-overview.component';
+import { Component, Input, AfterViewInit, OnChanges } from '@angular/core';
+import { ProcessTreeService } from 'src/app/services/processTreeService/process-tree.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-activity-overview-info-bar',
+  template: ` <div class="info-bar ps-2 pe-2">
+    <span class="float-end"
+      >activities in model: {{ numActivitiesInModel }} out of
+      {{ totalActivities }}</span
+    >
+  </div>`,
+  styleUrls: ['./info-bar.component.scss'],
+})
+export class InfoBarComponent implements AfterViewInit, OnChanges {
+  @Input()
+  activityFields: ActivityField[];
+
+  numActivitiesInModel: Number;
+  totalActivities: Number;
+
+  private _destroy$ = new Subject();
+
+  constructor(private processTreeService: ProcessTreeService) {}
+
+  ngOnChanges(): void {
+    this.totalActivities = this.activityFields.length;
+  }
+
+  ngAfterViewInit(): void {
+    // Handle change of current activies in the loaded model
+    this.processTreeService.activitiesInCurrentTree$
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((activitiesInTree) => {
+        this.numActivitiesInModel = this.activityFields.filter((act) =>
+          activitiesInTree.has(act.activityName)
+        ).length;
+      });
+  }
+}
