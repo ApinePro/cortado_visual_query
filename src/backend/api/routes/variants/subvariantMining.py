f66c5f85bbaa28c41606546ee8048889f36d0d5f
Ariba <102295115+ariba-work@users.noreply.github.com>
2023-11-08 10:43:25 +0100

    removed imports

--- src/backend/api/routes/variants/subvariantMining.py
+++ src/backend/api/routes/variants/subvariantMining.py
@@ -38,11 +38,8 @@ from cortado_core.subprocess_discovery.subtree_mining.folding_label import (
 
 import cache.cache as cache
 import numpy as np
-from cortado_core.variant_pattern_replications.pair import Pair
 
-from cortado_core.variant_pattern_replications.repetition_pairs import generate_consecutive_pairs, generate_dummy_tree
-
-from cortado_core.subprocess_discovery.subtree_mining.maximal_connected_components.maximal_connected_check import check_if_valid_tree
+from cortado_core.variant_pattern_replications.repetition_pairs import generate_consecutive_pairs, filter_overlapping_invalid_patterns, filter_maximal_patterns
 
 router = APIRouter(tags=["subvariantMining"], prefix="/subvariantMining")
 
@@ -204,34 +201,12 @@ def mineRepetitionPatterns(bid: int):
     k_patterns_filtered = DefaultDict(set)
     pairs_filtered = DefaultDict()
 
-    for k in ks: 
-        for tp in k_patterns[k]: 
-            if len(tp.tree.children) > 1 and check_if_valid_tree(tp.tree):
-              pair = create_pair(tp, treeBank[0].tree)
-              if not pair.__overlapping__():
-                k_patterns_filtered[k].add(tp)
-                pairs_filtered[tp] = pair
-
-    set_maximaly_closed_patterns(k_patterns_filtered)
-
-    for k in ks: 
-      for tp in k_patterns_filtered[k]: 
-         print(tp)
-         if tp.maximal:
-            pairs_from_kpatterns.add(pairs_filtered[tp])
-
-    # pairs_from_kpatterns = set()
-    # print("tree patterns: ")
-    # for _, patterns in k_patterns.items():
-    #   tree_patterns = list(patterns)
-    #   print(tree_patterns)
-    #   for _, treepat in enumerate(tree_patterns):
-    #      if len(treepat.tree.children) > 1 and treepat.maximal and check_if_valid_tree(treepat.tree):
-    #         pair = create_pair(treepat, treeBank[0].tree)
-    #         pairs_from_kpatterns.add(pair)
+    pairs_filtered, kpatterns_filtered = filter_overlapping_invalid_patterns(k_patterns, ks, treeBank[0].tree)
+    set_maximaly_closed_patterns(kpatterns_filtered)
+    pairs_from_kpatterns = filter_maximal_patterns(kpatterns_filtered, pairs_filtered, ks)
     
     print("pairs from k patterns: ")
     print(pairs_from_kpatterns)
     combined_pairs = pair_unions(pairs_from_kpatterns, single_act_pairs)
-    result = sorted(combined_pairs, key=lambda x: x.positions.dfs[1] - x.positions.dfs[0], reverse=True)
-    return sorted(result, key=lambda x: x.positions.dfs[1] - x.positions.dfs[0], reverse=True)
+    # result = sorted(combined_pairs, key=lambda x: x.positions.bfs[1] - x.positions.bfs[0], reverse=True)
+    return sorted(combined_pairs, key=lambda x: x.positions.bfs[1] - x.positions.bfs[0], reverse=True)
