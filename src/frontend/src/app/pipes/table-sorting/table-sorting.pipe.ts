e89c40cc0ca2d77e58708db97a0ab8f6c9bcd891
Weiran <weiran.yang@fit.fraunhofer.de>
2023-11-21 17:58:41 +0100

    rename tableSortingPipe and VariantInfoViewExplorer

---
+++ src/frontend/src/app/pipes/table-sorting/table-sorting.pipe.ts
@@ -0,0 +1,36 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ActivityField } from 'src/app/components/activity-overview/activity-overview.component';
+
+@Pipe({
+  name: 'tableSorting',
+})
+export class TableSortingPipe implements PipeTransform {
+  transform(array: any, sortKey: string, ascending: boolean): any {
+    if (!Array.isArray(array)) {
+      throw new Error('Input Data is not a sortable Array');
+    }
+
+    // Reverse behavior for string keys
+    if (
+      sortKey == 'activityName' ||
+      sortKey == 'case_id' ||
+      sortKey == 'earliest_time' ||
+      sortKey == 'latest_time' ||
+      sortKey == 'total_duration'
+    ) {
+      ascending = !ascending;
+    }
+
+    array.sort((a: ActivityField, b: ActivityField) => {
+      if (a[sortKey] < b[sortKey]) {
+        return ascending ? -1 : 1;
+      } else if (a[sortKey] > b[sortKey]) {
+        return ascending ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+
+    return array;
+  }
+}
