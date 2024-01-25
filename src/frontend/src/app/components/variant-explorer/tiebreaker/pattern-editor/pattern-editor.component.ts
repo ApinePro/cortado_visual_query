06e86b882703bce60495e4253e9602220ccf3ae0
Weiran <weiran.yang@fit.fraunhofer.de>
2024-01-25 13:39:56 +0100

    make the resize effect on button response faster

--- src/frontend/src/app/components/variant-explorer/tiebreaker/pattern-editor/pattern-editor.component.ts
+++ src/frontend/src/app/components/variant-explorer/tiebreaker/pattern-editor/pattern-editor.component.ts
@@ -178,7 +178,6 @@ export class PatternEditorComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   checkButtonCollapse() {
-    console.log(this.toolBar.nativeElement.offsetWidth);
     if (this.toolBar.nativeElement.offsetWidth < 766) {
       //according to the width of toolbar
       this.collapse = true;
